1bc566eefe70b00d4db3b1c8ff167cb9e3bbb738
Hriday Das <dashriday856@gmail.com>
2026-09-10 19:14:13 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # hashmap = {}
+        # if len(s) != len(t):
+        #     return False
+        # for i in range(len(s)):
+        #     hashmap[s[i]] = hashmap.get(s[i], 0) + 1
+        # for i in range(len(t)):
+        #     if t[i] in hashmap:
+        #         hashmap[t[i]] -= 1
+        #         if hashmap[t[i]] == 0:
+        #             del hashmap[t[i]] 
+        #     else:
+        #         return False
+        # else:
+        #     return True
+
+        hashmap = {}
+        hashmapp = {}
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            hashmap[s[i]] = hashmap.get(s[i], 0) + 1
+        for i in range(len(t)):
+            hashmapp[t[i]] = hashmapp.get(t[i], 0) + 1
+        if hashmap == hashmapp:
+            return True
+        else:
+            return False
+
+        
